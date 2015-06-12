62173e84154ece4cc967058eff857c8ef7b1cdd3
RinatSh <rinatsh2005@ukr.net>
2015-06-12 16:38:20 +0200

    add customer tab and list like news

---
+++ app/code/community/Neklo/Blog/Block/Customer.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Customer Tab block
+ */
+class Neklo_Blog_Block_Customer extends Mage_Core_Block_Template
+{
+
+    public function getCustomerLikeNews()
+    {
+        $customer = Mage::getSingleton('customer/session')->getCustomer();
+        if ($customer->getId()) {
+            return $customer;
+        }
+
+        return false;
+    }
+
+    /**
+     * Collection like customer news
+     *
+     * @return object
+     */
+
+    public function getCollection()
+    {
+        $customer = Mage::getSingleton('customer/session')->getCustomer();
+
+        if ($customer->getId()) {
+
+            $collection = Mage::getModel('neklo_blog/like')->getCollection();
+            $collection->addFieldToFilter('customer_id', $customer->getId());
+            $collection->getSelect()
+                ->joinLeft(array('news' => 'neklo_blog_news'), 'news.entity_id = main_table.news_id');
+
+        }
+
+        return $collection;
+    }
+
+    public function getItemUrl($newsItemLike)
+    {
+
+        if ($newsItemLike->getIdentifierUrl()) {
+
+            return $this->getUrl($newsItemLike->getIdentifierUrl());
+
+        } else {
+
+            return $this->getUrl('*/*/view', array('id' => $newsItemLike->getId()));
+
+        }
+
+    }
+}
---
+++ app/code/community/Neklo/Blog/controllers/CustomerController.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Class Neklo_Blog_CustomerController
+ */
+
+class Neklo_Blog_CustomerController extends Mage_Core_Controller_Front_Action
+{
+
+    public function indexAction()
+    {
+        if (!Mage::getSingleton('customer/session')->isLoggedIn()) {
+
+            $this->_redirect('customer/account/login');
+            return;
+
+        }
+
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+
+}
